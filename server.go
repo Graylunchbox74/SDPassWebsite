0bb57af70c56410dd38b7d2954b3aac276ea249b
the-rileyj <rileyjohnson99@hotmail.com>
2018-04-09 17:46:32 -0500

    Fixed merge conflict

--- server.go
+++ server.go
@@ -21,47 +21,11 @@ const (
 
 var db *sql.DB
 var tpl *template.Template
-var errorChannel chan locationalError
-
-type locationalError struct {
-	Error                 error
-	Location, Sublocation string
-}
-
-//checking error functions
-//check if there was an error
-func checkErr(err error) {
-	if err != nil {
-		println(err)
-	}
-}
-
-func checkLogError(location, sublocation string, err error) {
-	if err != nil {
-		logError(location, sublocation, err)
-	}
-}
-
-func logError(location, sublocation string, err error) {
-	errorChannel <- locationalError{err, location, sublocation}
-}
-
-func errorDrain() {
-	var lErr locationalError
-	for {
-		select {
-		case lErr = <-errorChannel:
-			fmt.Println(lErr.Location, lErr.Sublocation, lErr.Error)
-			//Handle Error Logging Here
-		}
-	}
-}
 
 func init() {
 	var err error
 	_, ok := os.LookupEnv("NODB")
 	if !ok {
-		db, err = sql.Open("sqlite3", "./userDatabase.db?_busy_timeout=5000")
 		if err != nil {
 			panic(err)
 		}
@@ -76,7 +40,6 @@ func init() {
 }
 
 func main() {
-	go errorDrain()
 	r := gin.Default()
 
 	//Route for the static files in www/
